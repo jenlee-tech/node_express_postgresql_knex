c5ef2001b41df8d7e6ad4dac5d31731b39320ade
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-15 23:33:13 -0400

    testing first commit

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: "",
+  },
+};
