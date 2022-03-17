2b0f4ddac1f41680d0273a568e8438bf86b99165
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-17 08:41:48 -0400

    created seed object in knexfile and then the seed files

--- src/db/migrations/20220317115914_productsAddPriceAndChangeProductNameToProductTitle.js
+++ src/db/migrations/20220317115914_productsAddPriceAndChangeProductNameToProductTitle.js
@@ -1,7 +1,3 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function (knex) {
   return knex.schema.table("products", (table) => {
     table.renameColumn("product_name", "product_title");
@@ -9,10 +5,6 @@ exports.up = function (knex) {
   });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function (knex) {
   return knex.schema.table("products", (table) => {
     table.renameColumn("product_title", "product_name");
