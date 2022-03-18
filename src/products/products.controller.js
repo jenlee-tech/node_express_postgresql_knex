9f558aee044438aa6d265626f776a9844b22efd2
Jennifer Lee <jenlee.tech@gmail.com>
2022-03-17 23:30:40 -0400

    created service for products resource

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,11 +1,13 @@
+const productsService = require("./products.service");
 function read(req, res, next) {
   res.json({ data: { product_title: "some product title" } });
 }
 
 function list(req, res, next) {
-  res.json({
-    data: [{ product_title: "product 1" }, { product_title: "product 2" }],
-  });
+  productsService
+    .list()
+    .then((data) => res.json({ data }))
+    .catch(next);
 }
 
 module.exports = {
---
+++ src/products/products.service.js
@@ -0,0 +1,8 @@
+const knex = require("../db/connection");
+function list() {
+  return knex("products").select("*");
+}
+
+module.exports = {
+  list,
+};
